0efe7be93406e1a1161f181e11b265a5f70001ce
José Neto <joseneto.jn1606@gmail.com>
2024-08-26 18:45:35 -0300

    feat: adding the task entity

---
+++ src/main/java/br/com/joseneto/to_do_list/domain/entity/Task.java
@@ -0,0 +1,22 @@
+package br.com.joseneto.to_do_list.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "taks")
+@Table(name = "tasks")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "priority")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String description;
+    private boolean completed;
+    private Integer priority;
+}
