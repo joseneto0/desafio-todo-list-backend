56c310ab897dd49a2b049509fd90ab8c32a0ae43
José Neto <joseneto.jn1606@gmail.com>
2024-08-26 18:45:56 -0300

    feat: adding the task controller

---
+++ src/main/java/br/com/joseneto/to_do_list/controller/TaskController.java
@@ -0,0 +1,52 @@
+package br.com.joseneto.to_do_list.controller;
+
+import br.com.joseneto.to_do_list.domain.entity.Task;
+import br.com.joseneto.to_do_list.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/task")
+public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping
+    public List<Task> viewAll(){
+        return taskService.findAllTasks();
+    }
+
+    @GetMapping("/{id}")
+    public Task viewTaskById(@PathVariable Long id) throws Exception {
+        return taskService.findTaskById(id);
+    }
+
+    @PostMapping("/create")
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
+    }
+
+    @PutMapping("/update/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task task) throws Exception {
+        return taskService.updateTask(id, task);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public HttpStatus deleteTask(@PathVariable Long id) throws Exception {
+        return taskService.deleteTask(id);
+    }
+
+    @GetMapping("/ordered")
+    public List<Task> orderedByPriority() {
+        return taskService.orderedByPriority();
+    }
+
+    @GetMapping("/completed/{id}")
+    public Task completedTask(@PathVariable Long id) throws Exception {
+        return taskService.completeTask(id);
+    }
+}
