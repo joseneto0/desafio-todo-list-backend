979f872aa1d3a9f7c649435635ed434cdeb6bcd8
José Neto <joseneto.jn1606@gmail.com>
2024-08-27 00:23:05 -0300

    refactor: changing the task service script, less code and more functionality

--- src/main/java/br/com/joseneto/to_do_list/service/TaskService.java
+++ src/main/java/br/com/joseneto/to_do_list/service/TaskService.java
@@ -4,6 +4,7 @@ import br.com.joseneto.to_do_list.domain.entity.Task;
 import br.com.joseneto.to_do_list.domain.repository.TaskRepository;
 import br.com.joseneto.to_do_list.service.comparator.TaskComparator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -16,9 +17,6 @@ public class TaskService {
     private TaskRepository taskRepository;
 
     public Task createTask(Task task) {
-        if (task.getPriority() == null){
-            task.setPriority(0);
-        }
         this.taskRepository.save(task);
         return task;
     }
@@ -31,38 +29,18 @@ public class TaskService {
         return this.taskRepository.findAll();
     }
 
-    public Task updateTask(Long id, Task task) throws Exception {
-        Task newTask = this.taskRepository.findById(id).orElseThrow(() -> new Exception("Task not found"));
-        newTask.setName(task.getName());
-        newTask.setDescription(task.getDescription());
-        newTask.setCompleted(task.isCompleted());
-        newTask.setPriority(task.getPriority());
-        this.taskRepository.save(newTask);
-        return newTask;
+    public Task updateTask(Task task) throws Exception {
+        this.taskRepository.save(task);
+        return task;
     }
 
     public HttpStatus deleteTask(Long id) throws Exception {
-        Task newTask = this.taskRepository.findById(id).orElseThrow(() -> new Exception("Task not found"));
-        this.taskRepository.delete(newTask);
+        this.taskRepository.deleteById(id);
         return HttpStatus.OK;
     }
 
     public List<Task> orderedByPriority(){
-        List<Task> tasks = this.taskRepository.findAll();
-        tasks.sort(new TaskComparator());
-        return tasks;
+        Sort listSorted = Sort.by("priority").descending();
+        return this.taskRepository.findAll(listSorted);
     }
-
-    public Task completeTask(Long id) throws Exception {
-        Task newTask = this.taskRepository.findById(id).orElseThrow(() -> new Exception("Task not found"));
-        newTask.setCompleted(true);
-        this.taskRepository.save(newTask);
-        return newTask;
-    }
-
-
-
-
-
-
 }
